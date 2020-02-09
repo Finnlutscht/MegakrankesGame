679c5328c190443c13125b08ed1a6c9c2d4a1f2a
Finnlutscht <60301892+Finnlutscht@users.noreply.github.com>
2020-02-09 15:18:53 +0100

    TimeSpan

---
+++ Usefull Phrases/TimeSpan.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Testitest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //Aktuelle und neue Uhrzeit
+            // DateTime aktuell = DateTime.Now;
+            //  DateTime spaeter = aktuell.AddDays(2);
+
+            //Berechne den Unterschied
+            // long zeitDiffTicks = spaeter.Ticks - aktuell.Ticks;
+            //  long zeitDiffStunden = zeitDiffTicks / TimeSpan.TicksPerHour;
+
+
+            //Gebe die Zeitdifferenz aus
+            // Console.WriteLine(zeitDiffStunden);
+            //  Console.ReadKey();
+
+            /*
+            TimeSpan zeitspanne = new TimeSpan(-10, 0, 0);
+
+            Console.WriteLine(zeitspanne);
+            Console.ReadKey();
+            Console.Beep();
+
+
+            Console.WriteLine(zeitspanne.Duration());
+            Console.ReadKey();
+            Console.Beep();
+
+
+
+            TimeSpan zeitspanne2 = new TimeSpan(20, 0, 0);
+
+            Console.WriteLine(zeitspanne2);
+            Console.ReadKey();
+            Console.Beep();
+
+            Console.WriteLine(zeitspanne2.Negate());
+            Console.ReadKey();
+            Console.Beep();
+            */
+
+
+            Laufer JuJu = new Laufer(19.24);
+            Laufer Maexchen = new Laufer(21.58);
+            Laufer Finn = new Laufer(17.91);
+
+            List<double> Zeitenliste = new List<double>();
+
+            Zeitenliste.Add(JuJu.LaufZeit);
+            Zeitenliste.Add(Maexchen.LaufZeit);
+            Zeitenliste.Add(Finn.LaufZeit);
+
+
+
+            foreach (double LaufZeit in Zeitenliste)
+            {
+                Console.WriteLine(LaufZeit);
+            }
+
+
+        }
+    }
+    
+
+
+
+}
