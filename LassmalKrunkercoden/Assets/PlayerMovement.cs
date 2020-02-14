ffbffcba20ca3c6959158c38f2036c7577a308d2
Finnlutscht <60301892+Finnlutscht@users.noreply.github.com>
2020-02-14 17:29:08 +0100

    j

--- LassmalKrunkercoden/Assets/PlayerMovement.cs
+++ LassmalKrunkercoden/Assets/PlayerMovement.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    public Joystick jsl;
+    public float fwws = 40f;
+    public float bwws = 20f;
+    public Rigidbody rb;
+
+
     void Update()
     {
-        
+        if (jsl.Vertical >= .2f)
+        {
+            rb.AddForce(0, 0, fwws * Time.deltaTime);
+        }
+        else if (jsl.Vertical <= -.2f)
+        {
+            rb.AddForce(0, 0, - fwws * Time.deltaTime);
+        }
+
+        if (jsl.Horizontal >= .2f)
+        {
+            rb.AddForce(fwws * Time.deltaTime, 0, 0);
+        }
+        else if (jsl.Horizontal <= - .2f)
+        {
+            rb.AddForce(- fwws * Time.deltaTime, 0, 0);
+        }
     }
 }
