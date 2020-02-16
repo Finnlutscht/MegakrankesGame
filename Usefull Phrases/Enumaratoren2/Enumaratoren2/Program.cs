bb1de63b6e3eaaf7839fb0d4c605abfd83aa1c07
Huntsman7 <julius.huendlings@outlook.de>
2020-02-16 20:37:28 +0100

    habe eine anwendung geschreiben wo enumaratoren hilfreich sind

---
+++ Usefull Phrases/Enumaratoren2/Enumaratoren2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Enumaratoren
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+            Wochentage tag1 = Wochentage.Montag;
+            for (int i = 0; i < 4; i++)
+            {
+
+                switch (tag1)
+                {
+                    case Wochentage.Montag:
+                        Console.WriteLine("Montag");
+                        break;
+
+                    case Wochentage.Dienstag:
+                        Console.WriteLine("Dienstag");
+                        break;
+                    case Wochentage.Mittwoch:
+                        Console.WriteLine("Mittwoch");
+                        break;
+
+
+                }
+                tag1++;
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+
+    //Wochentage als eigener Datentyp
+    //Alles bisle Leserlicher
+    enum Wochentage
+    {
+        Montag,
+        Dienstag = 2,
+        Mittwoch = 3,
+        Donnerstag = 4,
+        Freitag = 5,
+        Samstag = 6,
+        Sonntag = 7
+
+    }
+}
