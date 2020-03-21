9b5f2d4a4524873ed13348d25bb888f5ead0e0aa
Huntsman7 <julius.huendlings@outlook.de>
2020-03-21 12:50:48 +0100

    beweegung und verfolgung von spieler und gegner

---
+++ language support is important/Max ist ein pimmelkopf/Assets/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour { 
+
+        public CharacterController2D controller;
+
+        public float runspeed = 40f;
+        float horizontalMove = 0f;
+
+    // Start is called before the first frame update
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runspeed;
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+    }
+}
