fc1afc27e3aa87a8cc372ac13ce902f249ec747e
Huntsman7 <julius.huendlings@outlook.de>
2020-04-12 18:50:20 +0200

    spawn

---
+++ Archero/Archero/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public GameObject Enemyprefab;
+    public Transform point;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Q))
+        {
+            spawn();
+        }
+    }
+    public void spawn()
+    {
+        Vector3 pos = point.position;
+        Instantiate(Enemyprefab, pos, Quaternion.identity);
+    }
+}
